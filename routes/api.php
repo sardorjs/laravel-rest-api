192cf4c4a859422b2d8eb091cac23b3c93deb7e4
Sardor Saidov <sardor.saidov@alif.uz>
2024-03-21 23:06:51 +0500

    install api because of laravel 11 - and also 00:19:22 3.1 Versioning and Defining Routes

---
+++ routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\API\V1\CustomerController;
+use App\Http\Controllers\API\V1\InvoiceController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::prefix('v1')->namespace('App\Http\Controllers\API\V1')
+    ->group(function (){
+        Route::apiResource('customers', CustomerController::class);
+        Route::apiResource('invoices', InvoiceController::class);
+    });
